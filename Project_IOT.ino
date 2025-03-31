911341b05a56bb38df01e35acccc21ce60d426c3
tmthanhCT <mthanh29122004@gmail.com>
2025-03-31 12:57:06 +0700

    Add files via upload

--- Project_IOT.ino
+++ Project_IOT.ino
@@ -1,70 +1,59 @@
-#include <LiquidCrystal_I2C.h>
-#define BLYNK_PRINT Serial
-#define BLYNK_TEMPLATE_ID "TMPL6KA8cPH4z"
-#define BLYNK_TEMPLATE_NAME "ConnectWifi"
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+#include <LiquidCrystal.h>
 
-char auth[] = "lXcMQ5yrBfvugcXMv7KCJpg853_ZvZe8";
-char ssid[] = "LeakGasSystem"; 
-char pass[] = "11111111"; 
-BlynkTimer timer;
-int pinValue = 0;
-
-#define Buzzer D5
-#define Green D6
-#define Red D7
-#define Sensor A0
+//Declare LCD, Gas Sensor, Buzzer 
+//Declare led pins (RED, ORANGE, YELLOW, GREEN)
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int const GasSensor = A1;
+int buzzer = 10;
+int LED_GREEN = 7;
+int LED_YELLOW = 6;
+int LED_ORANGE = 8;
+int LED_RED = 9;
 
 void setup() {
-  Serial.begin(9600);
-  lcd.backlight();
-  lcd.init();
-  pinMode(Green, OUTPUT);
-  pinMode(Red, OUTPUT);
-  pinMode(Buzzer, OUTPUT);
-  pinMode(Sensor, INPUT);
-  Blynk.begin(auth, ssid, pass);
-  timer.setInterval(100L, notifiaction);
+  //Set the LED and buzzer pins as OUTPUT to control,
+    pinMode(LED_GREEN, OUTPUT);
+    pinMode(LED_YELLOW, OUTPUT);
+    pinMode(LED_ORANGE, OUTPUT);
+    pinMode(LED_RED, OUTPUT);
+    pinMode(buzzer, OUTPUT);
+    //start Serial communication (9600 baud) for debugging,
+    Serial.begin(9600);
+    //initialize 16x2 LCD display
+    lcd.begin(16, 2);
 }
-BLYNK_WRITE(V0) {
-  pinValue = param.asInt();
-}
-
-void notifiaction() {
-  int sensor = analogRead(Sensor);
-  Serial.println(sensor);
-  sensor = map(sensor, 0, 1024, 0, 100);
-  if (pinValue == 1) {
-    if (sensor <= 50) {
-      digitalWrite(Green, HIGH);
-      digitalWrite(Red, LOW);
-      digitalWrite(Buzzer, LOW);
-      lcd.setCursor(0, 1);
-      lcd.print("Gas value:Normal");
-    } else if (sensor > 50) {
-      Blynk.logEvent("gas_leak", "Warning! Gas leak detected");
-      digitalWrite(Green, LOW);
-      digitalWrite(Red, HIGH);
-      digitalWrite(Buzzer, HIGH);
-      lcd.setCursor(0, 1);
-      lcd.print("Gas value:High  ");
-    }
-    lcd.setCursor(0, 0);
-    lcd.print("Value : ");
-    lcd.print(sensor);
-    Blynk.virtualWrite(V1, sensor);
-  } else {
-    digitalWrite(Red, LOW);
-    digitalWrite(Buzzer, LOW);
-    digitalWrite(Green, LOW);
-    lcd.clear();
-  }
-}
-
 
 void loop() {
-  Blynk.run();
-  timer.run();
+    //Read value from gas sensor (GasSensor)
+    int value = analogRead(GasSensor);
+    //Convert sensor value (300-750) to 0-100 scale (%)
+    value = map(value, 300, 750, 0, 100);
+  //Control LEDs based on measured gas levels
+    digitalWrite(LED_GREEN, value <= 29 ? HIGH : LOW); //Safe
+    digitalWrite(LED_YELLOW, value >= 30 ? HIGH : LOW); //Alert
+    digitalWrite(LED_ORANGE, value >= 50 ? HIGH : LOW); //Danger
+    digitalWrite(LED_RED, value >= 80 ? HIGH : LOW); //Contaminated
+  //Clear LCD display before displaying new data 
+    lcd.clear();
+    lcd.setCursor(0, 0);
+  //Display pollution status on LCD screen and sound warning
+    if (value >= 80) {
+        lcd.print("CONTAMINATED"); //Severe Pollution
+        tone(buzzer, 80, 250); //Play Loud Warning Sound
+    } else if (value >= 50) {
+        lcd.print("DANGER"); //Danger
+        tone(buzzer, 60, 250); //Play medium warning sound
+    } else if (value >= 30) {
+        lcd.print("ALERT"); //Warning
+        tone(buzzer, 20, 250); //Play a small warning sound
+    } else {
+        lcd.print("SAFE"); //Safe environment
+    }
+    //Display gas value on second line of LCD display
+    lcd.setCursor(0,1);
+    lcd.print("Gas: ");
+    lcd.print(value);
+    lcd.print("%");
+  // Pause the program for 500ms before repeating
+    delay(500);
 }
